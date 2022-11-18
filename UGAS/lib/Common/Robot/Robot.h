57f6285d26ad359295fd8602c64108b78ca4053b
世羽 <sy2330@qq.com>
2022-11-18 19:40:34 +0800

    upload

---
+++ UGAS/lib/Common/Robot/Robot.h
@@ -0,0 +1,33 @@
+#pragma once
+/*
+Creation Date: 2022/11/18
+Latest Update: 2022/11/18
+Developer(s): 21-THY
+(C)Copyright: NJUST.Alliance - All rights reserved
+Header Functions:
+- 定义一个描述机器人及其装甲板运动信息的数据模型
+*/
+#include <opencv2/opencv.hpp>
+#include "../TimeStamp/TimeStampCounter.h"
+
+extern enum RotateDirc;
+
+class Robot {
+private:
+	TimeStamp _latestUpdate;
+	double pitch, roll, yaw;
+	cv::Vec3f _robotCenter;
+	cv::Vec3f _movingSpeed;
+
+	ArmorPlate _armor;
+
+	RotateDirc _rotate;
+	double _rotateSpeed; // omiga(w) : rad/s
+public:
+	Robot();
+
+	void Update(TimeStamp tp, ArmorPlate armor);
+	cv::Point3f Predict(int millisec);
+};
+
+extern Robot robots[10];
