69db5dbeba3c36792d6b6160c3d1ea0419502ed1
世羽 <sy2330@qq.com>
2022-11-24 12:24:27 +0800

    update

--- UGAS/Parameters/DebugSettings.h
+++ UGAS/Parameters/DebugSettings.h
@@ -6,6 +6,14 @@ Developer(s): 21-THY
 (C)Copyright: NJUST.Alliance - All rights reserved
 Header Functions:
 - 定义所有调试宏开关
+
+* 调试宏使用示例 (单变量)
+* DEBUG_GRAGH(var)
+* 调试宏使用示例 (多变量)
+* MAKE_GRAGH_DEFAULT
+*     GRAGH_ADD_VAR(var1, COLOR_YELLOW)
+*     GRAGH_ADD_VAR(var2, COLOR_BLUE)
+* SHOW_GRAGH(Gragh_var1_var2)
 */
 
 /// debug state switches 调试开关
@@ -23,15 +31,16 @@ Header Functions:
 
 /// static var			 静态变量
 #define VIDEO_VAR_TYPE	const char*			// 视频读入类型
-#define VIDEO_VAR		"Red_2.mp4" 		// 视频读入
+#define VIDEO_VAR		"Blue_2.mp4" 		// 视频读入
 #define NUM_PARA_TYPE	const char*			// 数字识别参数类型
 #define NUM_PARA		"V1.pb"				// 数字识别参数
 #define FILTER_TYPE		PID::PDfilter		// 目标运动滤波器类型
 
 
 /// debug var			 调试变量
-#define DEFAULT_TEAM	Blue 				// 虚拟云台队伍颜色
+#define DEFAULT_TEAM	Red 				// 虚拟云台队伍颜色
 #define NUM_DEFAULT		1					// 数字识别默认返回数字(一般是0)
+#define DEFAULT_MEM_NUM 300					// 调试曲线默认最大采样数量
 
 
 /// constant vars		 常值变量
@@ -41,17 +50,6 @@ Header Functions:
 #define PI				3.1415926535897		// Π值
 #define G				9.8					// 重力常数
 
-
-/// var(s) Debug func	调试曲线生成宏定义
-#define DEFAULT_MEM_NUM 300					// 默认最大采样数量
-/*# 调试宏使用示例 (单变量)
-* DEBUG_GRAGH(var)
-### 调试宏使用示例 (多变量)
-* MAKE_GRAGH_DEFAULT
-*     GRAGH_ADD_VAR(var1, COLOR_YELLOW)
-*     GRAGH_ADD_VAR(var2, COLOR_BLUE)
-* SHOW_GRAGH(Gragh_var1_var2)
-*/
 
 /// Time cost analysis	运行时间分析宏定义
 #define START_COUNT	{TimeStamp __tsTmp = TimeStampCounter::GetTimeStamp();
