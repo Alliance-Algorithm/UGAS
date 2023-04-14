2256d9eee481bdcb2775c83e67158175fcdeb673
soulde <fakesoulde@gmail.com>
2023-04-14 20:19:54 +0800

    add usb device rules files and install commands

--- cmake/FindHikCameraSDK.cmake
+++ cmake/FindHikCameraSDK.cmake
@@ -4,11 +4,11 @@ if (DEFINED ENV{MVCAM_SDK_PATH})
 else ()
     message(FATAL_ERROR "Do Not Found SDK. Please Install SDK And Run 'set_env_path.sh' To Set Environment Variable.")
 endif ()
+
 file(GLOB HiKCameraSDK_INCLUDES ${SDK_ROOT}/include/*.h)
 
 if (CMAKE_SIZEOF_VOID_P EQUAL 8)
     set(HikCameraSDK_LIB_DIR ${SDK_ROOT}/lib/64)
-
 else ()
     set(HikCameraSDK_LIB_DIR ${SDK_ROOT}/lib/32)
 endif ()
