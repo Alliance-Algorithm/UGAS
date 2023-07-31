8d8e078c0217e58f3bc54529194017f7fb69f5dc
qzhhhi <qzhhh@foxmail.com>
2023-07-31 22:25:04 +0800

    Add some new option on build.sh & deploy.sh. Fix package.xml.

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -7,6 +7,26 @@ set -e
 cd "${0%/*}"
 cd ..
 
+FORCE_INSTALLATION="OFF"
+while [ "$1" != "" ]; do
+    case "$1" in
+        "--force" )
+            FORCE_INSTALLATION="ON" ;;
+        "-f" )
+            FORCE_INSTALLATION="ON" ;;
+        "--build-arg" )
+            shift
+            break ;;
+        * )
+            echo "error: $1: invaild option"
+            echo "avaliable options:"
+            echo "    '--force' or '-f' to force installation."
+            echo "    '--build-arg' will pass all subsequent arguments to build.sh"
+            exit 1 ;;
+    esac
+    shift
+done
+
 REMOTE_IP="192.168.234.1"
 REMOTE_PORT="22"
 REMOTE_USERNAME="alliance"
@@ -15,17 +35,12 @@ REMOTE_PASSWORD_ROOT="alliance"
 
 echo "uploading to remote host..."
 /bin/sshpass -p "${REMOTE_PASSWORD}" /bin/rsync -av -e "/bin/ssh -p ${REMOTE_PORT}" --exclude-from='./.gitignore' . ${REMOTE_USERNAME}@${REMOTE_IP}:/home/${REMOTE_USERNAME}/ugas
-/bin/sshpass -p "${REMOTE_PASSWORD}" /bin/ssh ${REMOTE_USERNAME}@${REMOTE_IP} "/home/${REMOTE_USERNAME}/ugas/shell/build.sh"
+/bin/sshpass -p "${REMOTE_PASSWORD}" /bin/ssh ${REMOTE_USERNAME}@${REMOTE_IP} "/home/${REMOTE_USERNAME}/ugas/shell/build.sh $@"
 
-if [ "$1" = "--force" ] || [ "$1" = "-f" ]
+if [ "${FORCE_INSTALLATION}" == "ON" ]
 then
     /bin/sshpass -p "${REMOTE_PASSWORD_ROOT}" /bin/ssh root@${REMOTE_IP} "/home/${REMOTE_USERNAME}/ugas/shell/install.sh"
 else
-    if [ "$1" = "" ]
-    then
     /bin/sshpass -p "${REMOTE_PASSWORD_ROOT}" /bin/ssh root@${REMOTE_IP} "systemctl restart ugas.service || /home/${REMOTE_USERNAME}/ugas/shell/install.sh"
-    else
-        echo "warning: $1: invaild option."
-        echo "avaliable option: '--force' or '-f' to force installation."
-    fi
 fi
+
