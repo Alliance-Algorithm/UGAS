c0c01b11124ad10f44dbabb668c7f1597a53d595
SaXi-Yohane <55011641+SaXi-Yohane@users.noreply.github.com>
2023-10-05 18:56:38 +0800

    Revert "Merge pull request #6 from IrumaMegumi/develop"
    This reverts commit 2096f0ea1394a6308b25e850a24fd4506a22c635, reversing
    changes made to 55c904b35280b1439506f0ce08798a1b7df3ef9a.

--- src/Core/Identifier/Number/NumberIdentifier_V1.h
+++ src/Core/Identifier/Number/NumberIdentifier_V1.h
@@ -7,11 +7,9 @@ Developer(s): 22-Qzh 22-Ljc
 Header Functions:
 - 数字识别V1版本
 - 使用OTSU和CNN
-- 加入了模板匹配区分正负样本功能，具体情况需要再测，我写的是先用神经网络识别，后模板匹配，如果上车效果不好，需要将二者调换顺序，对于噪声较大图片，模板匹配的得分一般在0.3-0.5之间
 */
 
 #include <opencv2/opencv.hpp>
-#include <vector>
 
 #include "Core/Identifier/Armor/ArmorStruct.h"
 #include "Core/Identifier/NumberIdentifierInterface.h"
@@ -19,7 +17,6 @@ Header Functions:
 class NumberIdentifier_V1 {
 private:
     cv::dnn::Net _net;
-    std::vector<cv::Mat> _templateFigures;//模板图像
 public:
     explicit NumberIdentifier_V1(const char* model);
     NumberIdentifier_V1(const NumberIdentifier_V1&) = delete;
